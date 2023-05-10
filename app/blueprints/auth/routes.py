38a75ac387709d79fae4e3fc1306cb4739458c4c
Md Mosharofe Hossen <mdmosharofe@gmail.com>
2023-05-10 02:17:24 -0400

    added user form and database

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,11 +1,32 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
+from app.models import User 
+from app import db
+
 
 from . import bp 
+from app.forms import RegisterForm
 
-@bp.route('/register')
+
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.jinja')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        user = User.query.filter_by(username=username).first()
+        email = User.query.filter_by(email=form.email.data).first()
+        if not email and not user:
+            u = User(username=username,email=form.email.data,password=form.password.data)
+            u.commit()
+            flash(f"{username} registered")
+            return redirect(url_for("main.home"))
+        if user:
+            flash(f'{username} already exist, please try again')
+        else:
+            flash(f'{form.email.data} already exist, please login')
+    return render_template('register.jinja', form=form)
 
 @bp.route('/signin')
 def signin():
     return render_template('signin.jinja')
+
